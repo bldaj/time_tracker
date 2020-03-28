7e9a2a004db4460b52d47c675e4920e804de6f70
Konstantin <ws.kkaloshin@gmail.com>
2020-03-29 00:06:16 +0300

    Added DB connection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 
-from PyQt5 import QtWidgets
+from PyQt5 import (
+    QtSql, QtWidgets
+)
 
 import design
 
@@ -10,7 +12,26 @@ class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.startButton.clicked.connect()
+        self.connect_db()
+        self.startButton.clicked.connect(self.get_rows)
+
+    def connect_db(self):
+        db = QtSql.QSqlDatabase.addDatabase('QPSQL')
+        db.setHostName('localhost')
+        db.setPort(5432)
+        db.setDatabaseName('time_tracker')
+        db.setUserName('time_tracker')
+        db.setPassword('password')
+
+        if db.isOpen():
+            db.close()
+
+        if not db.open():
+            raise Exception("Error opening database: {0}".format(db.lastError().text()))
+
+    def get_rows(self):
+        query = QtSql.QSqlQuery()
+        print(query.exec_("select * from time_tracker;"))
 
 
 def main():
